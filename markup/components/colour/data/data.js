34224cebb19b65598fb7e97d423b5df88244cb6e
angelpsy <angelpsymail@gmail.com>
2017-01-04 13:36:42 +0500

    Верстка блока Colour и правки адаптива вышестоящих блоков

---
+++ markup/components/colour/data/data.js
@@ -0,0 +1,49 @@
+var data = {colour: {
+    items: [
+        {
+            title: 'Design quality',
+            text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut adipisicing. labore et dolore',
+            linkText: 'Find Out more',
+            background: {
+                color: '#f5989d'
+            }
+        },
+        {
+            title: 'Built to last',
+            text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut adipisicing. labore et dolore',
+            linkText: 'Find Out more',
+            background: {
+                color: '#615f7d'
+            }
+        },
+        {
+            title: 'Our clients',
+            text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut adipisicing .',
+            linkText: 'Get insights',
+            background: {
+                color: '#0b2438',
+                img: 'img-client.jpg'
+            },
+            icon: 'heart-empty'
+        },
+        {
+            mod: 'big',
+            title: 'Sign up & see why',
+            text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut adipisicing.',
+            linkText: 'Get Started Now',
+            background: {
+                color: '#4c2623',
+                img: 'img-sign-up.jpg'
+            },
+            icon: 'heart-empty'
+        },
+        {
+            title: 'Perfect pictures',
+            text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut adipisicing.',
+            linkText: 'Find Out more',
+            background: {
+                color: '#82ca9c'
+            }
+        }
+    ]
+}};
