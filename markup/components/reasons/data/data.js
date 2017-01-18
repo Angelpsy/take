6932faa17d753d538a6190c4ddb18bd5b6fd7005
A.Alekseev <angelpsymail@gmail.com>
2017-01-18 22:42:24 +0500

    Верстка блока reasons

---
+++ markup/components/reasons/data/data.js
@@ -0,0 +1,16 @@
+var data = {reasons: {
+    items: [
+        {
+            title: 'Web based',
+            text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. '
+        },
+        {
+            title: 'Team login',
+            text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. '
+        },
+        {
+            title: 'Great Support',
+            text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. '
+        }
+    ]
+}};
