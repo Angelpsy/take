32589c83a8431d7ae6572a3c5b3a392c4bdbddc7
A.Alekseev <angelpsymail@gmail.com>
2017-02-12 13:44:47 +0500

    Верстка блока с ценами

---
+++ markup/components/prices/data/data.js
@@ -0,0 +1,16 @@
+var data = {prices: {
+    items: [
+        {
+            label: 'Legacy',
+            title: 'Go it solo',
+            text: '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco</p>',
+            price: 9,
+        },
+        {
+            label: 'Most Popular',
+            title: 'Team Plan',
+            text: '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco</p>',
+            price: 18,
+        },
+    ]
+}};
