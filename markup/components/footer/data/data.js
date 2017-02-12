368f737e4bf2550667fc846527bc461d1145a74e
A.Alekseev <angelpsymail@gmail.com>
2017-02-12 14:56:28 +0500

    Верстка подвала, мелкие правки блока с ценами

---
+++ markup/components/footer/data/data.js
@@ -0,0 +1,79 @@
+var data = {footer: {
+    footerMenus: [
+        {
+            title: 'Resources',
+            items: [
+                {
+                    title: 'Tour',
+                    url: '#',
+                },
+                {
+                    title: 'Customers',
+                    url: '#',
+                },
+                {
+                    title: 'Pricing and Plans',
+                    url: '#',
+                },
+                {
+                    title: 'New Features',
+                    url: '#',
+                },
+                {
+                    title: 'Education',
+                    url: '#',
+                },
+            ]
+        },
+        {
+            title: 'Features',
+            items: [
+                {
+                    title: 'Tour',
+                    url: '#',
+                },
+                {
+                    title: 'Customers',
+                    url: '#',
+                },
+                {
+                    title: 'Pricing and Plans',
+                    url: '#',
+                },
+                {
+                    title: 'New Features',
+                    url: '#',
+                },
+                {
+                    title: 'Education',
+                    url: '#',
+                },
+            ]
+        },
+        {
+            title: 'How To`s',
+            items: [
+                {
+                    title: 'Tour',
+                    url: '#',
+                },
+                {
+                    title: 'Customers',
+                    url: '#',
+                },
+                {
+                    title: 'Pricing and Plans',
+                    url: '#',
+                },
+                {
+                    title: 'New Features',
+                    url: '#',
+                },
+                {
+                    title: 'Education',
+                    url: '#',
+                },
+            ]
+        },
+    ]
+}};
